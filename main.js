458df7e2bc984660ad9ec9ff668e92a205971943
Aida Nurgaeva <aida@MacBook-Pro-Aida.local>
2022-06-07 12:41:21 +0600

    added get request

--- main.js
+++ main.js
@@ -13,20 +13,42 @@ GET-получение данных
 CRUD -Create(post) Read(get) Update(put/patch) Delete(delete)
  */
 const API = "http://localhost:8000/todos";
-
+// ! CREATE
 let inpAdd = document.getElementById("inp-add");
 let btnAdd = document.getElementById("btn-add");
 // console.log(inpAdd, btnAdd);
-btnAdd.addEventListener("click", function () {
+btnAdd.addEventListener("click", async function () {
   let newToDo = {
     todo: inpAdd.value,
   };
   // console.log(newToDo);
-  fetch(API, {
+  if (newToDo.todo.trim() === "") {
+    alert("заполните полe");
+    return;
+  }
+  await fetch(API, {
     method: "POST",
     body: JSON.stringify(newToDo),
     headers: {
       "Content-type": "application/json; charset=utf-8",
     },
   });
+  inpAdd.value = "";
+  getTodos();
 });
+// ! READ
+let list = document.getElementById("list");
+console.log(list);
+async function getTodos() {
+  let response = await fetch(API)
+    .then(res => res.json())
+    .catch(err => console.log(err));
+  console.log(response);
+  list.innerHTML = "";
+  response.forEach(item => {
+    let newElem = document.createElement("div");
+    newElem.innerHTML = `<span>${item.todo}</span>`;
+    list.append(newElem);
+  });
+}
+getTodos();
