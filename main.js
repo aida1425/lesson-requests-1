94c60442f73f18a103200b7d9cafa682c3ba06c5
Aida Nurgaeva <aida@MacBook-Pro-Aida.local>
2022-06-07 11:43:27 +0600

    added post requests

--- main.js
+++ main.js
@@ -9,3 +9,24 @@ GET-получение данных
 /*
 команда для запуска данных json-server json-server -w db.json -p 8000
  */
+/*
+CRUD -Create(post) Read(get) Update(put/patch) Delete(delete)
+ */
+const API = "http://localhost:8000/todos";
+
+let inpAdd = document.getElementById("inp-add");
+let btnAdd = document.getElementById("btn-add");
+// console.log(inpAdd, btnAdd);
+btnAdd.addEventListener("click", function () {
+  let newToDo = {
+    todo: inpAdd.value,
+  };
+  // console.log(newToDo);
+  fetch(API, {
+    method: "POST",
+    body: JSON.stringify(newToDo),
+    headers: {
+      "Content-type": "application/json; charset=utf-8",
+    },
+  });
+});
