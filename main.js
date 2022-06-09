adaa039663be658f9f7ac98789779757c4edac51
Aida Nurgaeva <aida@MacBook-Pro-Aida.local>
2022-06-09 12:54:52 +0600

    added paginate search update

--- main.js
+++ main.js
@@ -45,6 +45,18 @@ btnAdd.addEventListener("click", async function () {
   getTodos();
 });
 // ! READ
+//! SEARCH
+
+let inpSearch = document.getElementById("inp-search");
+// console.log(inpSearch);
+inpSearch.addEventListener("input", function () {
+  console.log("input!");
+  getTodos();
+});
+//! PAGINATION
+
+let pagination = document.getElementById("pagination");
+let page = 1;
 // получаем элемент, чтоб в нем отобразить все таски
 let list = document.getElementById("list");
 // проверяем в консоли, чтоб убедиться что в переменной list сейчас НЕ пусто
@@ -52,10 +64,17 @@ let list = document.getElementById("list");
 // функция для получения всех тасков и отображения их  в div#list
 // async await нужен здесь для того чтобы при отправке запроса мы сначала получили данные и только потом записали все в переменную response, иначе (если мы не дождемся) туда запишется pending (состояние промиса который еще не выполнен)
 async function getTodos() {
-  let response = await fetch(API) // если не указать метод запросаю  то по умолчанию это GET запрос
+  let response = await fetch(
+    `${API}?q=${inpSearch.value}&_page=${page}&_limit=2`
+  ) // если не указать метод запроса  то по умолчанию это GET запрос
     .then(res => res.json()) // переводим все в json формат
     .catch(err => console.log(err)); // отловим ошибку
   // console.log(response);
+  let allTodos = await fetch(API)
+    .then(res => res.json())
+    .catch(err => console.log(err));
+  // console.log(allTodos.length / 2);
+  let lastPage = Math.ceil(allTodos.length / 2);
   // очищаем div#list чтоб список тасков корректно отображался и не хранил там предыдущие html-элементы со старыми данными
   list.innerHTML = "";
   // перебираем полученный из дб.жсон массив и для каждого обьекта из этого массива создаем div и задаем ему содержимое через метод innerHTML, каждый созданный элемент аппендим(добавялем) в div#list
@@ -64,13 +83,49 @@ async function getTodos() {
     newElem.id = item.id;
     newElem.innerHTML = `
     <span>${item.todo}</span>
-    <button class = "btn-delete">Delete</button>`;
+    <button class = "btn-delete">Delete</button>
+    <button class = "btn-edit">Edit</button>`;
     list.append(newElem);
   });
+  pagination.innerHTML = `
+  <button id="btn-prev" ${page === 1 ? "disabled" : ""}>Prev</button>
+  <span>${page}</span>
+  <button ${page === lastPage ? "disabled" : ""} id="btn-next">Next</button>
+  `;
 }
 //вызываем функцию чтоб как только откроется страница что-то было отображено
 getTodos();
+
+let modalEdit = document.getElementById("modal-edit");
+let modalEditClose = document.getElementById("modal-edit-close");
+let inpEditTodo = document.getElementById("inp-edit-todo");
+let inpEditId = document.getElementById("inp-edit-id");
+let btnSaveEdit = document.getElementById("btn-save-edit");
+// console.log(inpEditTodo, inpEditId, btnSaveEdit);
+// console.log(modalEditClose);
+// console.log(modalEdit);
+modalEditClose.addEventListener("click", function () {
+  modalEdit.style.display = "none";
+});
+
+btnSaveEdit.addEventListener("click", async function () {
+  let editedTodo = {
+    todo: inpEditTodo.value,
+  };
+  let id = inpEditId.value;
+  await fetch(`${API}/${id}`, {
+    method: "PATCH", // указываем метод
+    body: JSON.stringify(editedTodo), // указываем что именно нужно запостить
+    headers: {
+      "Content-type": "application/json; charset=utf-8",
+    }, // кодировка
+  });
+  modalEdit.style.display = "none";
+  getTodos();
+});
+
 document.addEventListener("click", async function (e) {
+  // !DELETE
   if (e.target.className === "btn-delete") {
     let id = e.target.parentNode.id;
     await fetch(`${API}/${id}`, {
@@ -78,5 +133,27 @@ document.addEventListener("click", async function (e) {
     });
     getTodos();
   }
+  // ! UPDATE (edit)
+  if (e.target.className === "btn-edit") {
+    modalEdit.style.display = "flex";
+    let id = e.target.parentNode.id;
+    // console.log(id);
+    let response = await fetch(`${API}/${id}`)
+      .then(res => res.json())
+      .catch(err => console.log(err));
+    // console.log(response);
+    inpEditTodo.value = response.todo;
+    inpEditId.value = response.id;
+  }
+
   // console.log(e.target.parentNode.id);
+  //! Pagination
+  if (e.target.id === "btn-next") {
+    page++;
+    getTodos();
+  }
+  if (e.target.id === "btn-prev") {
+    page--;
+  }
+  getTodos();
 });
